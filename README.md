e1b5f9fdf78e1bc28d75bf26650cc636387a35d3
Li jiamei <jli479@my.centennialcollege.ca>
2022-10-17 00:00:23 -0400

    made changes to overall layout

---
+++ README.md
@@ -0,0 +1,4 @@
+File: Portfolio
+Student Name: Li Jiamei
+Student Number: 301239326
+Date: 2022-10-16
--- routes/index.js
+++ routes/index.js
@@ -13,23 +13,23 @@ router.get('/home', function(req, res, next) {
 
 /* GET About page. */
 router.get('/about', function(req, res, next) {
-  res.render('index', { title: 'About',});
+  res.render('about', { title: 'About',});
 });
 
-/* GET Products page. */
-router.get('/products', function(req, res, next) {
-  res.render('index', { title: 'Products',});
+/* GET Projects page. */
+router.get('/projects', function(req, res, next) {
+  res.render('projects', { title: 'projects',});
 });
 
 /* GET Services page. */
 router.get('/services', function(req, res, next) {
-  res.render('index', { title: 'Services',});
+  res.render('services', { title: 'Services',});
 });
 
 
 /* GET Contact Us page. */
 router.get('/contact', function(req, res, next) {
-  res.render('index', { title: 'Contact Us',});
+  res.render('contact', { title: 'Contact Us',});
 });
 
 module.exports = router;
